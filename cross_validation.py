8be94d8bea4b4eac18e5a16721b76c4a84e3c6d3
harsh-choudhary1 <160221948+harsh-choudhary1@users.noreply.github.com>
2025-04-25 14:02:39 +0530

    Add files via upload

--- cross_validation.py
+++ cross_validation.py
@@ -1,4 +1,16 @@
+import numpy as np
+import pandas as pd
 from sklearn.datasets import load_diabetes
-x,y = load_diabetes(return_X_y=True , as_frame=True)
-x.head() 
-print (x.shape)
+
+X,y = load_diabetes(return_X_y=True , as_frame=True)
+X.head() 
+print (X.shape)
+
+rng = np.random.RandomState(42)
+n_random_features = 14
+X_random = pd.DataFrame(
+    rng.randn(X.shape[0], n_random_features),
+    columns=[f"random_{i:02d}" for i in range(n_random_features)],
+)
+X = pd.concat([X, X_random], axis=1)
+X[X.columns[::3]].head()
